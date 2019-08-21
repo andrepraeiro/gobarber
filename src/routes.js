e8251f12331ede95eb70632f5f304e40e036c46f
Andre Praeiro <andre.campos@sigma.com.br>
2019-08-21 15:41:56 -0400

    config sequelize, eslint, first migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({ message: "Hello World, by rocket" });
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello World, by rocket' });
 });
 
 export default routes;
