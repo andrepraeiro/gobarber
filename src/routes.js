ee353233e7743c23f0f3bc77f9487cf4b77406f9
Andre Praeiro <andrepraeiro@gmail.com>
2019-08-22 20:36:32 -0400

    feat: user store

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Andre Praeiro',
-    email: 'andrepraeiro@gmail.com',
-    password_hash: '12345454545',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
