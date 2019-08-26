b3af2588bf35135d3a65bf980b9b62591d76f3fd
Andre Praeiro <andrepraeiro@gmail.com>
2019-08-25 22:50:41 -0400

    feat: appointment

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const uploads = multer(multerConfig);
@@ -19,6 +20,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers/', ProviderController.index);
+
+routes.post('/appointments/', AppointmentController.store);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
