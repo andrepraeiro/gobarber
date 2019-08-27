bd96c67c6220a0d733f770139ac982b7f552fdbe
Andre Praeiro <andrepraeiro@gmail.com>
2019-08-26 20:20:51 -0400

    chore: config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '71e1167df19fc1',
+    pass: '13c5997a0efb73',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
