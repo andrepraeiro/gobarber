01b428c9af515821f2efa19263d95403a45e2ab7
Andre Praeiro <andre.campos@sigma.com.br>
2019-10-14 09:14:42 -0400

    persist authentication

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -7,16 +8,18 @@ import './config/reactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 
-import store from './store';
+import { store, persistor } from './store';
 import GlobaStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobaStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobaStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
