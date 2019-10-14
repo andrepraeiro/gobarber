4ed49cf9f59518eb196a9d886f8a82cc3c6da467
Andre Praeiro <andre.campos@sigma.com.br>
2019-10-14 10:21:01 -0400

    showing toasts

--- frontend-web/src/store/modules/auth/sagas.js
+++ frontend-web/src/store/modules/auth/sagas.js
@@ -1,4 +1,6 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
 import api from '~/services/api';
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -12,13 +14,14 @@ export function* signIn({ payload }) {
     });
     const { token, user } = response.data;
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique os seus dados');
     yield put(signFailure());
   }
 }
