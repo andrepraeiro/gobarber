9bf8076e7ee028a8fb0e81c3b07bbdd87e0a5827
Andre Praeiro <andrepraeiro@gmail.com>
2019-10-15 17:41:01 -0400

    avatar input

---
+++ frontend-web/src/pages/Profile/AvatarInput/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useField } from '@rocketseat/unform';
+
+import { Container } from './styles';
+
+export default function AvatarInput() {
+  const { defaultValue, registerField } = useField('avatar');
+  const [preview, setPreview] = useState();
+
+  function handleChange(e) {}
+
+  return (
+    <Container>
+      <label htmlFor="avatar">
+        <img src="" alt="" />
+        <input
+          type="file"
+          id="avatar"
+          accept="image/*"
+          onChange={handleChange}
+        />
+      </label>
+    </Container>
+  );
+}
