ba4bba84d29f05da3d60ac254c48ef1045ef9d9b
andrepraeiro <andrepraeiro@gmail.com>
2019-10-16 22:23:38 -0400

    dashboard styling

--- frontend-web/src/pages/Dashboard/index.js
+++ frontend-web/src/pages/Dashboard/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <div />;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>André Praeiro</span>
+        </Time>
+        <Time>
+          <strong>09:00</strong>
+          <span>André Praeiro</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>André Praeiro</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>André Praeiro</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
